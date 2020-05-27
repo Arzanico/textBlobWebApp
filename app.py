d8cb60e89f61e16d2df26f5a2a8722d63b5d376a
Nico <arzanico@hotmail.com>
2020-05-27 18:57:44 +0800

    Updating project from pycharm

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import os
 from unicodedata import normalize
 
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/input/', methods=['POST'])
 def input():
-
     if request.method == 'POST':
         usuario = request.form['usuario']
         tipoAnalisis = request.form['tipoAnalisis']
@@ -22,6 +22,7 @@ def input():
             return render_template('index.html' , message = 'Ingrese nombre de usuario')
 
         return render_template('input.html' , usuario=usuario , tipoAnalisis=tipoAnalisis)
+
 
 app.config["FILES_UPLOADS"] = "static/files"
 
@@ -67,27 +68,21 @@ tagDict = {
 @app.route('/proced/', methods=['POST'])
 def proced():
     if request.method == 'POST':
-
         elecc = request.form['choise']
 
         def giveMeText(algo):
             choise = algo
-
             if choise == 'texto':
                 text = request.form['texto']
                 return text
             if choise == 'archivo':
                 archivo = request.files['archivo']
                 archivo.save(os.path.join(app.config["FILES_UPLOADS"] , archivo.filename))
-
                 directorio = '{}/{}'.format(app.config["FILES_UPLOADS"], archivo.filename)
-
                 with open(directorio, 'r') as textFile:
                     text = textFile.read()
                     text = normalize('NFC', text)
-
                 return text
-
         #Uso a funcion para simplificar la llamada a la libreria TextBlob            
         texto = giveMeText(elecc)
         # Llamo a la mi clase donde imlemento la libreria TextBlob
@@ -102,6 +97,7 @@ def proced():
 @app.route('/volver', methods=['POST'])
 def vovler():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.debug = True
